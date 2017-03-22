cbaac84b2dc3c90b656ad35fecfd8c56574b6860
Aisore <evgenia15678a@yandex.ru>
2017-03-22 17:16:32 +0300

    Redone
    Delete status, image, username
    Added age

--- laba21python.py
+++ laba21python.py
@@ -4,35 +4,32 @@ import urllib
 import re
 from bs4 import BeautifulSoup
 
+my_file = open("some.txt", 'w') 
 number = random.randint(100000,999999)
 m = "https://vk.com/id{}".format(number)
-soup = BeautifulSoup(urllib.request.urlopen(m), 'lxml')
-for img in soup.find_all('img'):
-	if img.get('alt'):
-		print(img.get('alt'))
-		if ".jpg" in (img.get('src')):
-			print(img.get('src'))
-		else:
-			print('Too stubborn to show a photo')
-		break
-	else:
-		print("Doesn`t exist.")
-		break
-print(number)
+
 urllib.request.urlretrieve(url=m, filename='Account.html')
 infile = open('Account.html')
 lines = infile.readlines()
 for i in range(len(lines)):
-	line = lines[i]
-	if 'div class="pp_last_activity"' in line:
-		time = lines[i]
-		if 'Online' in time:
-			print('Online')
-		else:
-			print('Offline')
-	if 'div class="pp_status"' in line:
-		status = lines[i].strip('<div class="pp_status">').strip('</div>')
-		print(status)
-	else:
-		print('The victim has no status')
-		break
+    line = lines[i]
+    if 'div class="pp_last_activity"' in line:
+        time = lines[i]
+        if 'Online' in time:
+            #print('Online')
+            my_file.write("Online\n")
+        else:
+            #print('Offline')
+            my_file.write("Offline\n")
+    if 'div class="pp_info"' in line:
+        age = lines[i]
+        integ = ''
+        for n in age:
+            if n.isdigit():
+                integ += n
+            else:
+                integ += ' '
+        for i in integ.split():
+            print(i)
+            print(my_file.write(i))
+my_file.close()
