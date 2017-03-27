44ec1b3ba7c00c0581f3d44c159280bc668dc090
Aisore <evgenia15678a@yandex.ru>
2017-03-27 20:04:20 +0300

    Complete project rework
    Instead of the name, picture and status, program now collects age from
    the pages and then calculates the average age

--- laba21python.py
+++ laba21python.py
@@ -4,32 +4,35 @@ import urllib
 import re
 from bs4 import BeautifulSoup
 
-my_file = open("some.txt", 'w') 
-number = random.randint(100000,999999)
-m = "https://vk.com/id{}".format(number)
-
-urllib.request.urlretrieve(url=m, filename='Account.html')
-infile = open('Account.html')
-lines = infile.readlines()
-for i in range(len(lines)):
-    line = lines[i]
-    if 'div class="pp_last_activity"' in line:
-        time = lines[i]
-        if 'Online' in time:
-            #print('Online')
-            my_file.write("Online\n")
-        else:
-            #print('Offline')
-            my_file.write("Offline\n")
-    if 'div class="pp_info"' in line:
-        age = lines[i]
-        integ = ''
-        for n in age:
-            if n.isdigit():
-                integ += n
-            else:
-                integ += ' '
-        for i in integ.split():
-            print(i)
-            print(my_file.write(i))
+l = 0
+my_file = open("some.txt", 'a')
+while l < 3:
+    number = random.randint(100000,999999)
+    m = "https://vk.com/id{}".format(number)
+    urllib.request.urlretrieve(url=m, filename='Account.html')
+    infile = open('Account.html')
+    lines = infile.readlines()
+    for i in range(len(lines)):
+        line = lines[i]
+        if 'div class="pp_info"' in line:
+            age = lines[i]
+            integ = ''
+            ch = 0           
+            for n in age:
+                if n.isdigit():
+                    integ += n
+                else:
+                    integ += ' '
+            for i in integ.split():
+                l += 1
+                print(i)
+                my_file.write(i + "\n") #print age
 my_file.close()
+
+infile = open("some.txt", 'r')
+numbers = [float(w) for w in infile.read().split()]
+mean = round(sum(numbers)/len(numbers))
+mens = str(mean)
+my_file2 = open("Age.txt", 'w')
+my_file2.write("Средний возраст равен: ")
+my_file2.write(mens)
